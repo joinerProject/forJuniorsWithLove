0f1062644eccc9ff67c8e6f9081d363f01ddd30a
TehilaButterman <99127850+TehilaButterman@users.noreply.github.com>
2022-12-29 11:21:36 +0200

    change UI to mui

--- joiners-project/src/components/routes/routing.tsx
+++ joiners-project/src/components/routes/routing.tsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from "react-router-dom";
-import Auth from "../auth/auth";
+import SignIn from "../auth/login/login";
+import Register from "../auth/register/register";
+import CreateProject from "../create-project/create-main/createProject";
 
 const Router = (): JSX.Element => {
     return (
         <div className="Routing">
             <Routes>
-                <Route path="/" element={<Auth />} />
+                <Route path="/" element={<SignIn />} />
+                <Route path="/login" element={<SignIn />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/create-project" element={<CreateProject/>}/>
             </Routes>
         </div>
     );
